38a2b2b30e8dc993cf3e66aa8ff3a959a5efe311
Barber0 <1529095729@qq.com>
2017-05-15 23:05:01 +0800

    available改为status

---
+++ core/goods.php
@@ -0,0 +1,19 @@
+<?php
+require_once "./config.php";
+require_once "./utils.php";
+/**
+ *
+ * 撤回物品（物品下线）
+ * 
+ * @param
+ *      - $goods_id      (@INT) good_info的第一个字段
+ *      - $session_key  (@STRING)会话密钥 
+ * 
+ * @return
+ *      - $status       (@JSONStr)
+ *
+ **/
+revoke_goods($good_id, $session_key){
+
+}
+?>
