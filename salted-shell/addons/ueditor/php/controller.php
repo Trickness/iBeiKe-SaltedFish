55f18fdb7d26b5e59282cdd36e5b81767c11eca2
trickness <sternwzhang@outlook.com>
2017-09-11 09:36:54 +0800

    解决uploadimage中的timezone问题

--- salted-shell/addons/ueditor/php/controller.php
+++ salted-shell/addons/ueditor/php/controller.php
@@ -1,13 +1,12 @@
 <?php
 //header('Access-Control-Allow-Origin: http://www.baidu.com'); //设置http://www.baidu.com允许跨域访问
 //header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With'); //设置允许的跨域header
-date_default_timezone_set("Asia/chongqing");
+include "../../../config.php";
 
 header("Content-Type: text/html; charset=utf-8");
 
 $CONFIG = json_decode(preg_replace("/\/\*[\s\S]+?\*\//", "", file_get_contents("config.json")), true);
 $action = $_GET['action'];
-include "../../../config.php";
 include "../../../core/authorization.php";
 include "../../../core/utils.php";
 
