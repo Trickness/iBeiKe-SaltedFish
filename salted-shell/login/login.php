b60e8ca5325e2452ecfa3a6086ea663e15fd4fdc
Barber0 <1529095729@qq.com>
2017-10-23 00:45:08 +0800

    加入新的登陆页面

--- salted-shell/login/login.php
+++ salted-shell/login/login.php
@@ -2,177 +2,92 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>登陆</title>
-    <script src="../js/jquery-3.2.1.min.js"></script>
-    <style>
-        body
-        {
-            margin: 0;
-            background-image: url(../pic/backgroundimg.png);
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: center center;
-            background-attachment: fixed;
-        }
-        .header
-        {
-            width: 100%;
-            height: 70px;
-            background: #FFFFFF;
-            border-bottom-style: solid;
-            border-bottom-color:#95989A;
-            border-bottom-width: 1px;
-        }
-        .logo
-        {
-            position: absolute;
-            width: 50px;
-            height: 50px;
-            top: 11px;
-            left: 31px;
-        }
-        .shangcheng
-        {
-            position: absolute;
-            width: 144px;
-            height: 48px;
-            top: 12px;
-            left: 82px;
-        }
-        .a
-        {
-            color: #FF621C;
-            line-height: 48px;
-            padding-bottom: 8px;
-            transition-duration: 0.4s;
-        }
-        .a:hover
-        {   
-            font-size: 38px;
-            color: #ff0066;
-            border-bottom-color: #ff0066;
-            border-bottom-width: 3px;
-            border-bottom-style: solid;
-        }
-        input{
-            border:0px;
-        }
-        p{
-            color:white;
-            font-size:165%;
-        }
-        p.input_hint{
-            float:left;
-            line-height:0px;
-            text-align:right;
-            width:110px;
-        }
-
-        .lgn-box{
-            width: 500px;height: 550px;position: relative;top: -445px;
-        }
-        .lgn-box-below{
-            width: 500px;height: 515px;position: relative;top: 75px;
-            background-color: #FF4E00;
-            opacity: 0.5;
-            border-radius: 5px;
-        }
-        .input{
-            width: 340px;
-            height: 40px;
-            border-radius: 5px;
-            font-size: 20px;
-            margin-bottom: 35px;
-            padding-left: 15px;
-        }
-        #lgn label{
-            font-size: 20px;
-            color: white;
-        }
-        .Submit
-        {
-            width: 445px;
-            height: 50px;
-            border-width: 1px;
-            border-color: coral;
-            border-radius: 5px;
-            background: #FF621C;
-            font-family: Helvetica Neue;
-            font-size: 30px;
-            text-align: center;
-            color: #FFFFFF;
-            transition-duration: 0.4s;
-            bottom:  71px;
-        }
-        .Submit:hover
-        {
-            background-color: white;
-            color: #FF621C;
-        }
-        #motto{
-            color: white;
-            margin-top: 60px;
-        }
-    </style>
 </head>
 <body>
-    <?php
-        // require_once "../config.php";
-        // require_once "utils.php";
-        // require_once "users.php";
-        // require_once "authorization.php";
-        session_start();
-    ?>
-    <div class= "header">
-        <img src="../pic/beikelogo.png" class="logo">
-        <img src="../pic/beikeshop.png" class = "shangcheng">
-        <div style="float: right;margin-top: 13px;margin-right: 38px;font-family: Helvetica Neue;font-size: 20px;">
-            <a class="a" href="../signin/signin.php">注册</a>
-            <a class="a" href="../login/login.php">登陆</a>
-        </div> 
-    </div>
-    <center style="height: 600px;">
-            <div class="lgn-box-below"></div>
-            <div class="lgn-box">
-                <div id="motto">
-                    <h1>贝壳商城</h1>
-                    <p>
-                        让你的宝贝动起来
-                    </p>
-                </div>
-                <div id="status" style="display: none;color: yellow;">登陆失败！</div>
-                <div id="lgn" style="margin-top: 35px;">
-                    <label>学号：</label><input class="input" type="text" id="username" placeholder="  请输入你的学号" /><br>
-                    <label>密码：</label><input class="input" type="password" id="password" placeholder="  请输入你的密码" /><br>
-                    <input id="login" type="submit" name="sub" value="登陆" class="Submit" />
-                </div>
+    <?php include "../frame/head_user.php"; ?>
+
+    <style>
+        html,
+        body {
+        margin: 0;
+        padding: 0;
+        }
+
+        .background {
+        position: absolute;
+        display: block;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        }
+        @media(min-width:768px){
+            .login{background-color:white;opacity:0.8;padding:20px 20px 40px 20px;position:absolute;left:1000px;top:200px;width:350px;border:1px solid #cccccc;border-radius:5px;}
+        }
+    </style>
+    <div class="container" style="margin-top:180px;">
+        <div id="login" class="col-sm-4 col-sm-offset-4" style="background-color:white;opacity:0.9;border-radius:5px;border:1px solid #cccccc;padding:20px 40px 40px 40px;">
+            <div style="text-align:center;color:#fd9860;">
+                <h2>贝壳商城</h2>
             </div>
-    </center>
+            <div>
+                <div style="margin-top:20px;"><input type="text" class="form-control input-lg" placeholder="请输入您的用户名" v-model="username" /></div>
+                <div style="margin-top:30px;"><input type="password" class="form-control input-lg" placeholder="请输入您的密码" v-model="password" /></div>
+                <div style="margin-top:30px;"><button class="btn btn-lg btn-warning" style="width:100%;" @click="login">登陆</button></div>
+            </div>
+        </div>
+    </div>
+    <canvas class="background">
+    </canvas>
+    <script src="../js/pt/dist/particles.min.js"></script>
     <script>
-    $("#login").click(function(){
-        $.getJSON("../core/api-v1.php?action=login",{
-            username:$("#username").val(),
-            password:$("#password").val()
-        },function(data){
-            var status = data.status;
-            switch(status){
-                case "success":
-                    localStorage.session = document.cookie;
-                    window.location="../users/index.php";
-                    break;
-                case "failed":
-                    console.log(status);
-                    console.log(data.error);
-                    $("#status").css("display","block");
-                    break;
-                default:
-                    console.log(status);
-                    $("#status").css("display","block");
-                    break;
+        window.onload = function() {
+            var num = 0;
+            if (window.innerWidth > 768) {
+                num = 250;
+            }else{
+                num = 80;
             }
-                
-        });
-    });
+            Particles.init({
+                selector: '.background',
+                maxParticles:num,
+                connectParticles:true,
+                color:'#fd9860',
+            });
+        };
+        $(document).ready(function() {
+            var login = new Vue({
+                el:'#login',
+                data:{
+                    username:'',
+                    password:'',
+                },
+                methods:{
+                    login:function(){
+                        var t = this;
+                        $.getJSON('../core/api-v1.php?action=login',{username:t.username,password:t.password},function(data){
+                            var status = data.status;
+                            switch(status){
+                                case "success":
+                                    localStorage.session = document.cookie;
+                                    window.location="../users/index.php";
+                                    break;
+                                case "failed":
+                                    console.log(status);
+                                    console.log(data.error);
+                                    $("#status").css("display","block");
+                                    break;
+                                default:
+                                    console.log(status);
+                                    $("#status").css("display","block");
+                                    break;
+                            }
+                        });
+                    },
+                },
+            });
+        })
     </script>
 </body>
 </html>
